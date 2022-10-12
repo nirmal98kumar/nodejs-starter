f349ff18fdb68414659cf4d3c4345991561335ea
nirmal98kumar <54501378+nirmal98kumar@users.noreply.github.com>
2022-10-12 13:46:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ const app = express()   //load express application to variable
 
 const port = 3000
 
+const router = express.Router()
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-
-
+//router can be used for modularity
+router.get('/', (req, res) => {
+  res.send('Birds home page')
+})
 
 app.listen(port, () => {
   consoe.log(`Example app listening on port ${port}`)
